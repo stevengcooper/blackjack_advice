ed89cccaca97a1316f99dc9012897feb5ff486d0
Steven Cooper <stevengcooper76@gmail.com>
2016-02-03 16:24:13 -0500

    Converted Aces to 11.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,14 +1,20 @@
-puts "Welcome to the blackjack helper!"
-puts "Please enter Kings, Queens, and Jacks as 10, and Aces as A."
-puts "What is your first card?"
-card1 = gets.chomp
-puts "What is your second card?"
-card2 = gets.chomp
-puts "What is the dealer's card?"
-dealer_total = gets.chomp
+def blackjack_helper
+  puts "Welcome to the blackjack helper!"
+  puts "Please enter Kings, Queens, and Jacks as 10, and Aces as A."
+  puts "What is your first card?"
+  card1 = gets.chomp
+  puts "What is your second card?"
+  card2 = gets.chomp
+  puts "What is the dealer's card?"
+  dealer_total = gets.chomp
 
-card1 = 11 if card1 == "a"
-card2 = 11 if card2 == "a"
-dealer_total = 11 if dealer_total == "a"
-player_total = card1 + card2
-puts "Your total is #{player_total}"
+  if card1 == "A"
+    card1 = 11
+  end
+  if card2 == "A"
+    card2 = 11
+  end
+  player_total = card1.to_i + card2.to_i
+  puts "Your total is #{player_total}"
+end
+blackjack_helper
