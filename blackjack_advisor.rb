a90120b2b7804989100e9963d9631f77fa63e175
Steven Cooper <stevengcooper76@gmail.com>
2016-02-03 17:33:32 -0500

    adjusted the card check method and added an ability to use card names.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,3 +1,12 @@
+
+def check_card(card)
+  if card[0].downcase == "a"
+    card = 11
+  elsif card[0].downcase == "k" || card[0].downcase == "q" || card[0].downcase == "j"
+    card = 10
+  end
+end
+
 hash_h = Hash.new("Hit")
 hash_s = Hash.new("Stand")
 hash_sp = Hash.new("Split")
@@ -10,21 +19,14 @@ hash_hard = {5 => hash_h,
             }
 
 puts "Welcome to the blackjack helper!"
-puts "Please enter Kings, Queens, and Jacks as 10, and Aces as A."
 puts "What is your first card?"
 card1 = gets.chomp
 puts "What is your second card?"
 card2 = gets.chomp
 puts "What is the dealer's card?"
-dealer_total = gets.chomp
+dealer_card = gets.chomp
 
-def add_users_cards(card1,card2)
-  if card1 == "A"
-    card1 = 11
-  end
-  if card2 == "A"
-    card2 = 11
-  end
-  player_total = card1.to_i + card2.to_i
-end
-puts hash_h[5]
+check_card(card1)
+check_card(card2)
+check_card(dealer_card)
+puts hash_hard[8][6]
