8b4ee82f4e788bd797a795d4d5a27dc59618245a
Steven Cooper <stevengcooper76@gmail.com>
2016-02-04 00:24:26 -0500

    populated hash_pair. Adjusted check_card method.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,9 +1,12 @@
 
 def check_card(card)
-  if card[0].downcase == "a"
-    card = 11
-  elsif card[0].downcase == "k" || card[0].downcase == "q" || card[0].downcase == "j"
-    card = 10
+  first_letter = card[0].downcase
+  if first_letter == "a"
+    11
+  elsif ["k","q", "j"].include?(card)
+    10
+  else
+    card.to_i
   end
 end
 
@@ -45,20 +48,26 @@ hash_soft = {13 => hit.merge({4 => "Double down if possible. If not Hit.", 5 => 
             19 => stand.merge({6 => "Double down if possible.  If not Stand."}),
             20 => stand,
             21 => stand}
-hash_pair = {}
-puts "Welcome to the blackjack helper!"
+hash_pair = {4 => split.merge({8 => "Hit.", 9 => "Hit.", 10 => "Hit.", 11 => "Hit."}),
+            6 => split.merge({9 => "Hit.", 10 => "Hit.", 11 => "Hit."}),
+            8 => hit.merge({4 => "Here's a good chance to Split.", 5 => "Here's a good chance to Split.", 6=> "Here's a good chance to Split."}),
+            10 => double_hit.merge({10 =>  "You should Hit.", 11 => "You should Hit."}),
+            12 => split.merge({8 =>  "You should Hit.", 9 =>  "You should Hit.", 10 =>  "You should Hit.", 11 =>  "You should Hit."}),
+            14 => split.merge({9 =>  "You should Hit.", 10 => "You should Stand.", 11 =>  "You should Hit."}),
+            16 => split,
+            18 => split.merge({7 => "You should Stand.", 10 => "You should Stand.", 11 => "You should Stand."}),
+            20 => stand,
+            22 => split}
+
 puts "What is your first card?"
 card1 = gets.chomp
 puts "What is your second card?"
 card2 = gets.chomp
 puts "What is the dealer's card?"
 dealer_card = gets.chomp
-check_card(card1)
-check_card(card2)
-card1= card1.to_i
-card2= card2.to_i
-check_card(dealer_card)
-dealer_card = dealer_card.to_i
+card1 = check_card(card1)
+card2 = check_card(card2)
+dealer_card= check_card(dealer_card)
 user_card_total = card1.to_i + card2.to_i
 
 if  card1 == 11 || card2 == 11
@@ -68,4 +77,3 @@ elsif card1 == card2
 else
   puts hash_hard[user_card_total][dealer_card]
 end
-hash_soft[20][9]
